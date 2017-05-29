87ddc3274b7e5643474a48cebc090e723b658db3
erric rapsing <erric.rapsing@gmail.com>
2017-05-29 18:14:03 +0800

    code adjustments..

--- web/modules/employee-management/routes.js
+++ web/modules/employee-management/routes.js
@@ -1,32 +1,36 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const module_name = path.basename(__dirname);
+const Employees = require("./model/Employee");
+
+function lib(app, lib_name) {
+	return require(`${app.get("LIB_PATH")}/${lib_name}`);
+}
+
 module.exports = function(app) {
 	
-	const express = require("express");
-	const path = require("path");
-	const router = express.Router();
-	const module_name = path.basename(__dirname);
-	const url = require(`${app.get("LIB_PATH")}/prefixer`)(module_name).url;
-	
-	const Employees = require("./model/Employee");
-
 	console.log(module_name);
-	router.get(url("/"), function(req, res) {
+	let prefix = lib(app, "prefixer")(`/${module_name}`).prefix;
+	
+	router.get(prefix("/"), function(req, res) {
 		console.log("rendering...");
 		let template = `
 				<html>
 					<body>
 						<h3>Employee Management Home</h3>
-						<a href="${url("/register")}">Add New</a>
+						<a href="${prefix("/register")}">Add New</a>
 					</body>
 				</html>
 			`;
 		res.send(template);
 	});
 
-	router.get(url("/register"), function(req, res) {
+	router.get(prefix("/register"), function(req, res) {
 		let template = `
 				<html>
 					<body>
-						<form name="register" action="${url("/register")}" method="POST">
+						<form name="register" action="${prefix("/register")}" method="POST">
 							<input type="text" name="lastname" placeholder="lastname" />
 							<input type="text" name="firstname" placeholder="firstname" />
 							<input type="text" name="middlename" placeholder="middlename" />
@@ -38,7 +42,7 @@ module.exports = function(app) {
 		res.send(template);
 	});
 
-	router.post(url("/register"), Employees.register);
+	router.post(prefix("/register"), Employees.register);
 
 	app.use(router);
 };
